058d606fe1ea5c6f15acf8c80d00ac17218c8efc
Hemant Sirsat <hemantsirsat40@gmail.com>
2021-07-20 13:39:08 +0530

    Readme added

--- src/components/Home.js
+++ src/components/Home.js
@@ -181,7 +181,6 @@ export default class Home extends React.Component {
                         </div>
                         <div className="reset-container">
                             <button
-                                // disabled={   }
                                 onClick={()=>{
                                     this.setState({
                                         billAmount:0.00,
